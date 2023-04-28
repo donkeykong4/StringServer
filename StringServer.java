40fb6bee7fbc1ee79441c7a4db2c915e2574067d
Jason <jmpo@ucsd.edu>
2023-04-28 16:25:55 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,1 +1,23 @@
+import java.io.IOException;
+import java.net.URI;
 
+class Handler implements URLHandler {
+    String s = "";
+}
+
+public String handleRequest(URI url) {
+    if 
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
